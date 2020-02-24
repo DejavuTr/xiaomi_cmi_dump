7bae6e4aa5cef339483afc69df97014e16611ef2
AndroidDumps <AndroidDumps@github.com>
2020-02-24 14:33:01 +0000

    Add system for qssi-user 10 QKQ1.191117.002 20.2.21 release-keys
    Signed-off-by: AndroidDumps <AndroidDumps@github.com>

---
+++ system/system/bin/install-recovery.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:134217728:a2ee8bfda64f5793bee1b88d2869fc080c64a30c; then
+  applypatch  \
+          --patch /system/recovery-from-boot.p \
+          --source EMMC:/dev/block/bootdevice/by-name/boot:134217728:b61e2f4e32cf6865c75725f64ef24bc758e29e74 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery:134217728:a2ee8bfda64f5793bee1b88d2869fc080c64a30c && \
+      log -t recovery "Installing new recovery image: succeeded" && \
+        echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
+      (log -t recovery "Installing new recovery image: failed" && \
+        echo "Installing new recovery image: failed" > /cache/recovery/last_install_recovery_status)
+else
+  log -t recovery "Recovery image already installed" && \
+  echo "Recovery image already installed" > /cache/recovery/last_install_recovery_status
+fi
